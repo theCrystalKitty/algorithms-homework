4282b3e6dc45cb20e9ff3e92c462612245ba792c
theCrystalKitty <hafklib@gmail.com>
2024-10-14 12:59:25 +0300

    feature/InsertionSort

---
+++ homework1/insertionSort.ts
@@ -0,0 +1,14 @@
+var arr: Array<number> = [45, 22, 7, 11, 92, 1, 6, 88];
+console.log(arr.join(", "));
+
+for(let i: number = 0; i < arr.length; i++) {
+    var key: number = arr[i];
+    let j: number = i - 1
+    while(arr[j] > key && j >= 0) {
+        arr[j + 1] = arr[j]
+        j--;
+    }
+    arr[j + 1] = key;
+}
+
+console.log(arr.join(", "));
